3278d21cb8fa4d853b6c7c266e1c2c7d8f6b691b
calvarezflx <claudio.alvarez@flexxus.com.ar>
2026-07-14 21:20:15 -0300

    feat(auth): crear UserEntity y AuthResolver

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { AuthModule } from './auth/auth.module';
 
 
 
@@ -37,7 +38,9 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 
     SeedModule,
 
-    FilesModule
+    FilesModule,
+
+    AuthModule
   ],
 
 })
---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,31 @@
+import { IsArray, isArray, IsBoolean, isBoolean, IsString } from "class-validator";
+import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity('users')
+export class User {
+    @PrimaryGeneratedColumn('uuid')
+    id       !: string;
+
+    @Column('text', {
+        unique: true
+    })
+    email    !: string;
+
+    @Column('text')
+    password !: string;
+
+    @Column('text')
+    fullName !: string;
+
+    @Column('bool')
+    isActive !: boolean;
+
+    @Column('text', {
+        array: true,
+        default: ['user']
+    })
+    roles    !: string[];
+         
+
+}
