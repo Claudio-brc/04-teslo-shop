443aa1563409abfa724ce3acfce81461a0da97ab
calvarezflx <claudio.alvarez@flexxus.com.ar>
2026-07-20 21:13:00 -0300

    login first step

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,9 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
+import { LoginUserDto, CreateUserDto } from './dto';
 
 @Injectable()
 export class AuthService {
@@ -33,6 +33,25 @@ export class AuthService {
       this.handleDBErrors(error);
     }
   }
+
+  async login(loginUserDto: LoginUserDto){
+    const { password, email } = loginUserDto;
+    
+    const user = await this.userRepository.findOne({ 
+       where: { email },
+      select: { email: true, password: true}
+     });
+
+    if ( !user )
+      throw new UnauthorizedException('Credentials are not valid (email)');
+
+    if ( !bcrypt.compareSync(password, user.password))
+      throw new UnauthorizedException('Credentials are not valid (password)');
+
+    return user;
+
+    //todo return jwt
+  }  
 
   private handleDBErrors( error: any ): never {
     if (error.code === '23505') 
