2f00ecdcee0aa78c16c1bd4f353c6a3f034109bf
calvarezflx <claudio.alvarez@flexxus.com.ar>
2026-07-27 00:28:41 -0300

    JwtStrategy

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,43 @@
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User } from "../entities/user.entity";
+import { JwtPayLoad } from "../interfaces/jwt-payload.interface";
+import { InjectRepository } from "@nestjs/typeorm";
+import { ConfigService } from "@nestjs/config";
+import { Repository } from "typeorm";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy( Strategy ){
+  
+  constructor(
+    @InjectRepository( User )
+    private readonly userRepository: Repository<User>,
+
+    configService: ConfigService
+
+  ){
+
+    super({
+      secretOrKey: configService.get<string>('JWT_SECRET')!,
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+  
+  async validate(payload: JwtPayLoad): Promise < User >  {
+     
+    const { email } = payload;
+   
+    const user      = await this.userRepository.findOneBy( { email });
+
+    if (!user)
+      throw new UnauthorizedException('Token not valid')
+
+    if (!user.isActive)
+      throw new UnauthorizedException('User not active')
+   
+
+    return user;
+  }  
+
+}
