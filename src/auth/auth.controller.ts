28464dfff1d3b7199a6a3781caee2e2f1526f70a
calvarezflx <claudio.alvarez@flexxus.com.ar>
2026-07-30 23:54:13 -0300

    feat: add custom decorator to inject authenticated user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto/';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from './decorators/get-user.decorator';
+import { User } from './entities/user.entity';
 
 
 
@@ -21,10 +23,14 @@ export class AuthController {
 
   @Get('private')
   @UseGuards( AuthGuard())
-  testingPrivateRoute(){
+  testingPrivateRoute(
+    //@Req() request: Express.Request
+    @GetUser() user : User
+  ){
     return {
       ok: true,
-      message: 'hello world private'
+      message: 'hello world private',
+      user
     }
   }
  
