c92fc79fbaab5eee420df21c946f1cae60f654de
calvarezflx <claudio.alvarez@flexxus.com.ar>
2026-08-04 21:42:37 -0300

    feat: add role-based authorization guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req, SetMetadata } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto/';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from './entities/user.entity';
 import { RawHeaders, GetUser } from './decorators';
+import { UserRoleGuard } from './guards/user-role/user-role.guard';
 
 
 
@@ -37,6 +38,18 @@ export class AuthController {
       rawHeaders
     }
   }
- 
+
+  @Get('private2')
+  @SetMetadata('roles', ['admin', 'super-user'])
+  @UseGuards( AuthGuard(), UserRoleGuard )
+  privateRoute2(
+    @GetUser() user: User
+  ) {
+      return {
+        ok: true,
+        user
+      }
+    }
+  
 
 }
