c95540f1b294e3aaeb6e3f968eb3c3ad48d0b3f0
calvarezflx <claudio.alvarez@flexxus.com.ar>
2026-08-06 15:28:23 -0300

    refactor: introduce reusable role authorization decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,6 +5,8 @@ import { AuthGuard } from '@nestjs/passport';
 import { User } from './entities/user.entity';
 import { RawHeaders, GetUser } from './decorators';
 import { UserRoleGuard } from './guards/user-role/user-role.guard';
+import { RoleProtected } from './decorators/role-protected/role-protected.decorator';
+import { ValidRoles } from './interfaces';
 
 
 
@@ -40,7 +42,7 @@ export class AuthController {
   }
 
   @Get('private2')
-  @SetMetadata('roles', ['admin', 'super-user'])
+  @RoleProtected( ValidRoles.superUser, ValidRoles.admin)
   @UseGuards( AuthGuard(), UserRoleGuard )
   privateRoute2(
     @GetUser() user: User
