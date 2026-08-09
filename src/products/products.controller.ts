8724be653a81390220727a54f76955a220ec22f1
calvarezflx <claudio.alvarez@flexxus.com.ar>
2026-08-09 20:30:27 -0300

    feat: protect product and seed endpoints with role-based authorization

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -3,12 +3,15 @@ import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from '../common/dtos/pagination.dto';
+import { Auth } from '../auth/decorators';
+import { ValidRoles } from '../auth/interfaces';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @Auth( ValidRoles.user )
   create(@Body() createProductDto: CreateProductDto) {
     console.log('entroalcontroller');
     return this.productsService.create(createProductDto);
@@ -26,12 +29,14 @@ export class ProductsController {
   }
 
   @Patch(':id')
+  @Auth( ValidRoles.user )
   update(@Param('id') id: string, 
   @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(id, updateProductDto);
   }
 
   @Delete(':id')
+  @Auth( ValidRoles.admin )  
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.productsService.remove(id);
     }
